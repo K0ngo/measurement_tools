b9822690af35ff04fea69a5696007d8d317e3df8
Alexander Bakuta <>
2018-06-07 10:02:44 +0300

    init project

---
+++ measurement_tools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  measurement_tools
+//
+//  Created by Alexander Bakuta on 07.06.2018.
+//  Copyright © 2018 k0ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
